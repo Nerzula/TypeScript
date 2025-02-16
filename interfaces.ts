4e044a66bbc864e0c131f06b3244569405852359
Alex <alexzloid@gmail.com>
2025-02-16 10:56:21 +0300

    feat: add interfaces comments

--- interfaces.ts
+++ interfaces.ts
@@ -1,18 +1,34 @@
+//? interface – это специальная конструкция, которая задает шаблон (чертеж) для объектов.
+//? Она описывает, какие свойства и методы должен иметь объект и какого они типа.
+
 interface UserInfo {
 	phone: string;
 	email: string;
 }
 
+//* Описывает объект с двумя обязательными строковыми полями:
+//* phone: string
+//* email: string
 interface User extends UserInfo {
 	name: string;
 	surname: string;
 	hieght?: number;
 }
 
+//* Расширяет UserInfo, то есть, наследует его поля phone и email.
+//* Добавляет свои поля:
+//*   name: string
+//*   surname: string
+//*   hieght?: number – необязательное поле (в конце стоит ?).
+
 interface Customer {
 	user: User;
 	bill: number;
 }
+
+//* Описывает объект с двумя полями:
+//* user: User – вложенный объект типа User.
+//* bill: number – число.
 
 const customer: Customer = {
 	user: {
@@ -23,3 +39,7 @@ const customer: Customer = {
 	},
 	bill: 12312,
 };
+
+//* Объект customer соответствует интерфейсу Customer.
+//* Внутри вложенный объект user полностью соответствует интерфейсу User (необязательное поле hieght можно не указывать).
+//* Поле bill — число, как требуется.
